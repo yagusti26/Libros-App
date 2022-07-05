6f18282b311c0c881c756e1d9024b2674a73f8cf
yagusti26 <yasminagusti@gmail.com>
2022-07-04 22:37:43 -0300

    cart view!

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -4,18 +4,23 @@ import ItemDetail from './ItemDetail';
 
 const ItemDetailContainer = () => {
     const [product, setProduct] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
 
     useEffect(() => {
         traerProducto()
             .then((res) => {
                 setProduct(res);
+                setIsLoading(false);
             })
             .catch((error) => console.log(error));
     }, []);
 
     return (
         <div className='flex flex-wrap p-5 items-center justify-center'>
-            <ItemDetail product={product} />
+            { isLoading
+            ? <h3>Cargando...</h3>
+            : <ItemDetail product={product} />
+            }
         </div>
     );
 };
