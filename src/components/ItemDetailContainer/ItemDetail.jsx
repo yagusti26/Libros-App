82cd72d98ecdcbccde7dc1472a4abed70c1eb748
yagusti26 <yasminagusti@gmail.com>
2022-07-20 11:02:07 -0300

    debuggin

--- src/components/ItemDetailContainer/ItemDetail.jsx
+++ src/components/ItemDetailContainer/ItemDetail.jsx
@@ -7,7 +7,7 @@ import s from './Detail.module.css';
 // import { useNavigate } from 'react-router-dom';
 
 
-const ItemDetail = ({ product }) => {
+const ItemDetail = ({ item }) => {
     const { addToCart, cart, deleteItem, isOnCart } = useContext(cartContext);
     
     console.log(cart);
@@ -15,36 +15,36 @@ const ItemDetail = ({ product }) => {
     //let navigate = useNavigate();  para usar mas
 
     function handleAddtoCart(cantidad) {
-        addToCart(product, cantidad);
+        addToCart(item, cantidad);
     }
 
     return (
         <div className={s.containerDetail}>
             <div className={s.containerDetailImg}>
-                <img src={product.img} alt={product.name}  />
+                <img src={item.img} alt={item.name}  />
             </div>
             <div className={s.containerDetailInfo}>
-                <h3>{product.name}</h3>
-                <h4>{product.author}</h4>
-                <h4>$ {product.price}</h4>
-                <p>{product.description}</p>
+                <h3>{item.name}</h3>
+                <h4>{item.author}</h4>
+                <h4>$ {item.price}</h4>
+                <p>{item.description}</p>
 
                 {/* rendering conditional  */}
-                {product.stock <= 0 && <Error isWarning={true} text="Lo sentimos, no hay stock" />}
+                {item.stock <= 0 && <Error isWarning={true} text="Lo sentimos, no hay stock" />}
                 
-                {isOnCart(product.id) ? (
+                {isOnCart(item.id) ? (
                     <Link className='bg-green-500 py-2 px-8 rounded-md font-bold text-white'
                     to="/cart">
                         Ir al carrito
                     </Link>
 
                 ) : (
-                    <Counter stock={product.stock} initial={1} onAdd={handleAddtoCart} />
+                    <Counter stock={item.stock} initial={1} onAdd={handleAddtoCart} />
 
                 )}
                 
-                {isOnCart(product.id) && (
-                    <button onClick={() => deleteItem(product.id)} className='mt-3 bg-red-500 py-2 px-8 rounded-md font-bold text-white'>
+                {isOnCart(item.id) && (
+                    <button onClick={() => deleteItem(item.id)} className='mt-3 bg-red-500 py-2 px-8 rounded-md font-bold text-white'>
                         Eliminar del carrito
                     </button>
                 )}
